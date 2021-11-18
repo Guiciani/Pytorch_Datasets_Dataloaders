ad2f2f2c4f6810a6cf2899dfdfd70b6b7ff6f63e
Guilherme <guiciani@gmail.com>
2021-11-18 11:23:49 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+import os
+import pandas as pd
+from torchvision.io import read_image
+
+
+# Importando os dados no seguinte formato:
+# -> Root: caminho onde o train/test esta armazenado
+# -> train: especificando o caminho do treinamento
+# -> download=True: baixando os dados da internet se nao estiver na root
+# -> transform and target_transform -> Especifica as transformações da 
+# feature e label
+
+
+training_data = datasets.FashionMNIST(
+    root="data",
+    train=True,
+    download=True,
+    transform=ToTensor()
+)
+
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor()
+)
+
+# Interando e visualizando o dataset
+
+# Podemos indexar os Datasets manualmente como uma lista: training_data[index]. Usamos o matplotlib
+# para visualizar algumas amostras nos dados do treinamento
+# 
+
+labels_map = {
+    0: "T-Shirt",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle Boot",
+}
+
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="copper")
+plt.show()
+
+
+class CustomImageDataset(Dataset):
+    def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
+        self.img_labels = pd.read_csv(annotations_file)
+        self.img_dir = img_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
